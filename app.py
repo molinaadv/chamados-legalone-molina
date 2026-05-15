6333675ec10bbb1ce6f251dff583e829c2c724b9
molinaadv <alexandre.brito@molinaadvogado.adv.br>
2026-05-15 11:35:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ st.set_page_config(
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 query_params = st.query_params
 modo_tv = query_params.get("tv", "0") == "1"
